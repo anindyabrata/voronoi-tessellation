7a9df6c001aca6e5f24540ba3d92fe0201ed4c19
Anindya Brata Choudhury <anibchy@gmail.com>
2021-08-11 01:03:11 -0700

    Update main to test for input validation

--- app/devmain.cpp
+++ app/devmain.cpp
@@ -1,20 +1,15 @@
-// #include <GL/glew.h>
-// #include <GLFW/glfw3.h>
-// #include <glm/vec3.hpp>
-// 
-// #include <vector>
-// #include <iostream>
-
-#include "ogview/simviewer.hpp"
-#include "ogview/voronoi_viewable.hpp"
-using namespace std;
-using namespace ogview;
-
+#include "fsl3d/utils.cpp"
+#include "fsl3d/naive.cpp"
+//#include "ogview/simviewer.hpp"
+//#include "ogview/voronoi_viewable.hpp"
 
 int main(int argc, char** argv)
 {
-	VoronoiViewable vv;
-	SimulationViewer sv(vv);
-	sv.render();
+	auto site_list = fsl3d::utils::read_site_list();
+	//auto voronoi = fsl3d::generate_naive_voronoi(site_list);
+	//ogview::VoronoiViewable vv(voronoi);
+	//ogview::SimulationViewer sv(vv);
+	//sv.render();
+	//fsl3d::utils::write_voronoi();
 	return 0;
 }
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-rm -rf tmp/* && cmake -H. -Btmp -DCMAKE_BUILD_TYPE=Debug -DENABLE_ASAN=true -DENABLE_UBSAN=true && cmake --build tmp && ./tmp/devmain
+rm -rf tmp/* && cmake -H. -Btmp -DCMAKE_BUILD_TYPE=Debug -DENABLE_ASAN=true -DENABLE_UBSAN=true && cmake --build tmp && ./tmp/devmain < in.txt
