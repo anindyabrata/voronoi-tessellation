1e428d806914257ba870c4dfc5ebbab8115d5abd
Anindya Brata Choudhury <anibchy@gmail.com>
2021-08-15 13:10:01 -0700

    Implement shaders for different types of faces, edges and points

--- include/ogview/simviewer.hpp
+++ include/ogview/simviewer.hpp
@@ -10,15 +10,15 @@
 #include "ogview/voronoi_viewable.hpp"
 
 namespace ogview{
-	enum FaceGroup {completed, incomplete, beachline};
+	enum class FaceGroup {cell, sweepline, beachline};
 	class SimulationViewer{
 		public:
 		SimulationViewer(VoronoiViewable& _vv):vv(_vv){
-			auto &dverts = vv.getVertices();
-			for(int i = 0; i < dverts.size(); i += 3) std::cout << dverts[i] << " " << dverts[i+1] << " " << dverts[i+2] << std::endl;
-			auto &dfaces = vv.getCells();
-			assert(0 < dfaces.size());
-			for(int i = 0; i < dfaces.size(); ++i) { for(int j = 0; j < dfaces[i].size(); ++j) std::cout << dfaces[i][j] << " "; std::cout << std::endl; }
+			// auto &dverts = vv.getVertices();
+			// for(int i = 0; i < dverts.size(); i += 3) std::cout << dverts[i] << " " << dverts[i+1] << " " << dverts[i+2] << std::endl;
+			// auto &dfaces = vv.getCells();
+			// assert(0 < dfaces.size());
+			// for(int i = 0; i < dfaces.size(); ++i) { for(int j = 0; j < dfaces[i].size(); ++j) std::cout << dfaces[i][j] << " "; std::cout << std::endl; }
 			init();
 		}
 		~SimulationViewer(){
@@ -28,10 +28,10 @@ namespace ogview{
 			rend();
 		}
 		private:
-		VoronoiViewable vv;
+		VoronoiViewable& vv;
 		GLFWwindow* window = NULL;
 		bool wireframe_mode = false;
-		GLuint cell_tri_prog, cell_edge_prog;
+		GLuint site_prog, cell_prog, edge_prog, sweep_prog, beach_prog;
 		GLuint VAO, vertex_buffer;
 		linalg::aliases::float4x4 transform = linalg::identity;
 		void initData()
@@ -44,10 +44,21 @@ namespace ogview{
 				 void main()
 				 {
 				 gl_Position = mulmat * vec4(position, 1.0);
+				 gl_PointSize = 5.0;
 				 }
 				);
 
-			const char* cell_tri_frag_shader = GLSL
+			const char* site_frag_shader = GLSL
+				(
+				 450 core,
+				 out vec4 FragColor;
+				 void main()
+				 {
+				 FragColor = vec4(0.8, 0.6, 0.6, 0.4);
+				 }
+				);
+
+			const char* cell_frag_shader = GLSL
 				(
 				 450 core,
 				 out vec4 FragColor;
@@ -56,8 +67,7 @@ namespace ogview{
 				 FragColor = vec4(0.6, 1.0, 1.0, 0.1);
 				 }
 				);
-
-			const char* cell_edge_frag_shader = GLSL
+			const char* edge_frag_shader = GLSL
 				(
 				 450 core,
 				 out vec4 FragColor;
@@ -67,8 +77,32 @@ namespace ogview{
 				 }
 				);
 
-			cell_tri_prog = LoadProgram(vert_shader, NULL, cell_tri_frag_shader);
-			cell_edge_prog = LoadProgram(vert_shader, NULL, cell_edge_frag_shader);
+			const char* sweep_frag_shader = GLSL
+				(
+				 450 core,
+				 out vec4 FragColor;
+				 void main()
+				 {
+				 FragColor = vec4(0.2, 0.2, 0.8, 0.1);
+				 }
+				);
+
+			const char* beach_frag_shader = GLSL
+				(
+				 450 core,
+				 out vec4 FragColor;
+				 void main()
+				 {
+				 FragColor = vec4(0.2, 0.8, 0.2, 0.1);
+				 }
+				);
+
+			site_prog   = LoadProgram(vert_shader, NULL, site_frag_shader);
+			cell_prog   = LoadProgram(vert_shader, NULL, cell_frag_shader);
+			edge_prog   = LoadProgram(vert_shader, NULL, edge_frag_shader);
+			sweep_prog  = LoadProgram(vert_shader, NULL, sweep_frag_shader);
+			beach_prog  = LoadProgram(vert_shader, NULL, beach_frag_shader);
+
 
 			float data[] =
 			{
@@ -81,15 +115,12 @@ namespace ogview{
 				0,1,2
 			};
 
-			// dynamic VAO setup
+			// Dynamic VAO setup
 			glGenVertexArrays(1, &VAO);
 			glBindVertexArray(VAO);
 			glGenBuffers(1, &vertex_buffer);
 			glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
 			glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_DYNAMIC_DRAW);
-			//glGenBuffers(1, &dindex_buffer);
-			//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, dindex_buffer);
-			//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), indexes, GL_DYNAMIC_DRAW);
 			glEnableVertexAttribArray(0);
 			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 			glBindVertexArray(0);
@@ -129,14 +160,14 @@ namespace ogview{
 			// glEnable(GL_SAMPLE_ALPHA_TO_COVERAGE_ARB);
 			// glDepthFunc(GL_LEQUAL);
 
-			// https://andersriggelsen.dk/glblendfunc.php
+			// Used https://andersriggelsen.dk/glblendfunc.php to find a blending mode that works
 			glEnable(GL_BLEND);
 			// glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 			// glBlendEquation(GL_FUNC_SUBTRACT);
-			// glBlendFunc(GL_ONE_MINUS_SRC_COLOR, GL_ONE_MINUS_DST_COLOR);
-			// glBlendFunc(GL_SRC_COLOR, GL_DST_COLOR);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 			glBlendEquation(GL_FUNC_ADD);
+
+			glEnable(GL_PROGRAM_POINT_SIZE);
 
 			initData();
 		}
@@ -181,49 +212,86 @@ namespace ogview{
 				glfwPollEvents();
 				handleKeyboardEvents();
 
+				vv.setProgress(25);
+
 				// Dynamic draw
+				// Update dynamic vertices
 				glBindVertexArray(VAO);
 				glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
 				auto &vertices = vv.getVertices();
 				glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), &vertices[0], GL_DYNAMIC_DRAW);
 				// glBufferSubData?
-				auto &dfaces = vv.getCells();
 
-				// update dynamic vertices
 				// load faces (indices, face groups)
-				std::vector<std::vector<unsigned int>> faces;
-				for(int i = 0; i < dfaces.size(); ++i) faces.push_back(dfaces[i]);
+				auto &siteverts  = vv.getSiteIndices();
+				auto &cellfaces  = vv.getCells();
+				auto &sweepfaces = vv.getSweepline();
+				auto &beachfaces = vv.getBeachline();
+
+				// Place into single container for z-sorting
+				// std::vector<std::vector<unsigned int>> faces;
+				// std::vector<FaceGroup> groups;
+				// for(int i = 0; i <  cellfaces.size(); ++i) faces.push_back( cellfaces[i]), groups.push_back(FaceGroup::cell);
+				// for(int i = 0; i < sweepfaces.size(); ++i) faces.push_back(sweepfaces[i]), groups.push_back(FaceGroup::sweepline);
+				// for(int i = 0; i < beachfaces.size(); ++i) faces.push_back(beachfaces[i]), groups.push_back(FaceGroup::beachline);
+
 				// sort faces
-				linalg::aliases::float4 cpos = getTransformedCameraPos();
-				std::vector<float> dist;
-				for(auto face: faces){
-					float d = 0;
-					for(int i = 0; i < face.size(); ++i) {
-						int vi = face[i];
-						float vf, dd = 0;
-						for(int j = 0; j < 3; ++j){
-							vf = vertices[vi * 3 + j] - cpos[j];
-							vf *= vf;
-							dd += vf;
-						}
-						if(dd > d) d = dd;
-					}
-					dist.push_back(d);
+				// linalg::aliases::float4 cpos = getTransformedCameraPos();
+				// std::vector<float> dist;
+				// for(auto &face: faces){
+				//	float d = 0;
+				//	for(int i = 0; i < face.size(); ++i) {
+				//		int vi = face[i];
+				//		float vf, dd = 0;
+				//		for(int j = 0; j < 3; ++j){
+				//			vf = vertices[vi * 3 + j] - cpos[j];
+				//			vf *= vf;
+				//			dd += vf;
+				//		}
+				//		if(dd > d) d = dd;
+				//	}
+				//	dist.push_back(d);
+				// }
+				// std::vector<int> face_order; for(int i = 0; i < faces.size(); ++i) face_order.push_back(i);
+				// auto cmp = [dist](const int &a, const int &b) -> bool { return dist[a] > dist[b]; };
+				// std::sort(face_order.begin(), face_order.end(), cmp); // ASAN error
+
+				// Render sites
+				useProg(site_prog);
+				glDrawElements(GL_POINTS, siteverts.size(), GL_UNSIGNED_INT, siteverts.data());
+
+				for(auto &face: cellfaces){
+					useProg(cell_prog);
+					if(!wireframe_mode) glDrawElements(GL_TRIANGLE_STRIP, face.size(), GL_UNSIGNED_INT, face.data());
+					useProg(edge_prog);
+					glDrawElements(GL_LINE_LOOP, face.size(), GL_UNSIGNED_INT, face.data());
 				}
-				std::vector<int> face_order; for(int i = 0; i < faces.size(); ++i) face_order.push_back(i);
-				auto cmp = [dist](const int a, const int b) -> bool { return dist[a] > dist[b]; };
-				std::sort(face_order.begin(), face_order.end(), cmp);
-				assert(0 < face_order.size());
-				// show faces (if(face_group == 2) 
-				for(int fi: face_order){
-					assert(2 < faces[fi].size());
-					useProg(cell_tri_prog);
-					// if(!wireframe_mode) glDrawElements(GL_TRIANGLE_STRIP, 1 * 3, GL_UNSIGNED_INT, &faces[fi]);
-					if(!wireframe_mode) glDrawElements(GL_TRIANGLE_STRIP, faces[fi].size(), GL_UNSIGNED_INT, faces[fi].data());
-					useProg(cell_edge_prog);
-					//glDrawElements(GL_LINE_LOOP, 1 * 3, GL_UNSIGNED_INT, &faces[fi]);
-					glDrawElements(GL_LINE_LOOP, faces[fi].size(), GL_UNSIGNED_INT, faces[fi].data());
+				for(auto &face: sweepfaces){
+					useProg(sweep_prog);
+					glDrawElements(GL_TRIANGLE_STRIP, face.size(), GL_UNSIGNED_INT, face.data());
 				}
+				for(auto &face: beachfaces){
+					useProg(beach_prog);
+					glDrawElements(GL_TRIANGLE_STRIP, face.size(), GL_UNSIGNED_INT, face.data());
+				}
+
+				// Render faces and edges
+				// for(int fi: face_order){
+				//	if(groups[fi] == FaceGroup::cell){
+				//		useProg(cell_prog);
+				//		if(!wireframe_mode) glDrawElements(GL_TRIANGLE_STRIP, faces[fi].size(), GL_UNSIGNED_INT, faces[fi].data());
+				//		useProg(edge_prog);
+				//		glDrawElements(GL_LINE_LOOP, faces[fi].size(), GL_UNSIGNED_INT, faces[fi].data());
+				//	}
+				//	else if(groups[fi] == FaceGroup::sweepline){
+				//		useProg(sweep_prog);
+				//		glDrawElements(GL_TRIANGLE_STRIP, faces[fi].size(), GL_UNSIGNED_INT, faces[fi].data());
+				//	}
+				//	else{
+				//		useProg(beach_prog);
+				//		glDrawElements(GL_TRIANGLE_STRIP, faces[fi].size(), GL_UNSIGNED_INT, faces[fi].data());
+				//	}
+				// }
 				glBindVertexArray(0);
 
 				glFlush();
