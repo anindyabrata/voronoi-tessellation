51c358a7ea6d1667a4e72e04fc54a341e514d8d5
Anindya Brata Choudhury <anibchy@gmail.com>
2021-08-16 10:23:56 -0700

    Remove duplication from random number generation

--- app/rand_input.cpp
+++ app/rand_input.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <chrono>
+#include <set>
 #include <random>
 
 int main(){
-	unsigned int num;
-	std::cin >> num;
-	std::default_random_engine eng;
-	while(num--) std::cout << eng() << " " << eng() << " " << eng() << std::endl;
+	unsigned int count, seed = std::chrono::system_clock::now().time_since_epoch().count();
+	std::mt19937 g(seed);
+	std::cin >> count;
+	std::set<unsigned int> s;
+	while(s.size() < 3 * count) s.insert(g());
+	int i = 0;
+	for(auto n: s) {
+		std::cout << n * (long double) 1e-6L;
+		if(++i % 3 == 0) std::cout << std::endl;
+		else std::cout << " ";
+	}
 	return 0;
 }
