1cb9fe60a364f40f8c6b067dc993fe89c096ca42
Anindya Brata Choudhury <anibchy@gmail.com>
2021-08-16 05:47:01 -0700

    Remove unnecessary files

--- run.sh
+++
@@ -1,4 +0,0 @@
-# cmake -H. -Btmp_cmake -DCMAKE_INSTALL_PREFIX=~/id
-# cmake --build tmp_cmake --clean-first --target install
-# ~/id/bin/generate_input > in.txt
-rm -rf tmp/* && cmake -H. -Btmp -DCMAKE_BUILD_TYPE=Debug -DENABLE_ASAN=true -DENABLE_UBSAN=true && cmake --build tmp && ./tmp/demo < in.txt
